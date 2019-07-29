3d7c9e621ec727d3d9a51e093b7347658ba52c1a
menja-coder <rakotomenja@gmail.com>
2019-07-29 21:36:10 +0300

    Finalize

--- lib/controller.rb
+++ lib/controller.rb
@@ -14,8 +14,19 @@ class ApplicationController < Sinatra::Base
 		redirect'/'
 	end
 
-	get '/gossips/:id' do
+	get '/gossips/:id' do  #allows to find gossip via id
 	  erb :show, locals: {find_gossip: Gossip.find(params[:id])}
 	end
+
+	 # allows editing with the formual edit.erb
+  get '/gossips/:id/edit' do
+    erb :edit, locals: {id: params['id'].to_i, gossips: Gossip.find(params['id'].to_i)}
+  end
+
+  # retrieve the entry in the form to update the gossip
+  post '/gossips/edit/' do
+    Gossip.update(params['id'].to_i,params["gossip_author"], params["gossip_content"])
+    redirect '/'
+  end
 end
 
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -15,7 +15,7 @@ class Gossip
             end  
         end
 
-        def self.all
+        def self.all  #Methode which will show all gossip
             all_gossips = []
             CSV.read("./db/gossip.csv").each do |csv_line|
             all_gossips << Gossip.new(csv_line[0], csv_line[1])
@@ -23,9 +23,26 @@ class Gossip
             return all_gossips
         end
 
-        def self.find(id)
+        def self.find(id) #methode to find the author via his id
             
             return Gossip.all[id.to_i]
+        end
+
+        def self.update(id, author, content) #methode to edit the gossip
+            gossips = []
+            CSV.read("./db/gossip.csv").each_with_index do |row, index|
+                if id.to_i == (index)
+                    gossips << [author, content]
+                else 
+                    gossips << [row[0], row[1]]
+                end
+            end
+
+            CSV.open("./db/gossip.csv", "w") do |csv|
+                gossips.each do |row|
+                    csv << row
+                end
+            end
         end
 
 
